5810afef40b59684a0fca3b4cffa5f07b821ad52
BuildTools <unconfigured@null.spigotmc.org>
2022-03-11 18:44:22 -0600

    init

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 const open = require('open')
 app.get('/', (req, res) => {
-  res.send(`${port}, ${req}`)
+  res.send(`<h1 style="font-family: Arial, Helvetica, sans-serif;">Welcome to the Website! This is a test to see if it worked!</h1>`)
 })
 
 app.listen(port, () => {
